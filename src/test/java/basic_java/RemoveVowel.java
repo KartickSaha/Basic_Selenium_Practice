06cd3a637ad0eb61237de605fa9f77093fa27a02
Kartick <kartick01@yahoo.com>
2018-11-27 22:49:57 -0500

    Update a Java class as remove volwel with count vowel,consonats, digits,blanc spaces also

--- src/test/java/basic_java/RemoveVowel.java
+++ src/test/java/basic_java/RemoveVowel.java
@@ -10,11 +10,43 @@ public class RemoveVowel {
         String string = "Bangladesh";
         String lowerCase = string.toLowerCase();
         String removeVowel = lowerCase.replaceAll("[aeiou]","");
-        System.out.println(removeVowel);
+        System.out.println("Removel vowel one is " + removeVowel);
     }
     public static void removeVowel02(){
-        String string = "Bangladesh";
+        String string = "Bangladesh is my favourite country";
         String removeVowel = string.replaceAll("[aAeEiIoOuU]","");
-        System.out.println(removeVowel);
+        System.out.println("Removel vowel two is "+ removeVowel);
+
+
+        int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+        string = string.toLowerCase();
+        for (int i =0; i< string.length(); i++)
+
+        {
+            char ch = string.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i'
+                    || ch == 'o' || ch == 'u')
+            {
+                vowels++;
+            }
+            else if((ch >= 'a'&& ch <= 'z')) {
+                consonants++;
+            }
+            else if( ch >= '0' && ch <= '9')
+            {
+                digits++;
+            }
+            else if (ch ==' ')
+            {
+                spaces++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("White spaces: " + spaces);
     }
-}
+    }
+
